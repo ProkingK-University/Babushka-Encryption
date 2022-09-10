e2358cb7acec7f0ff69fc4a1c340dbb5e0d22b7f
Ashley Kapaso <ashleykapaso@gmail.com>
2022-09-10 18:02:44 +0200

    Controller Exception Impleamentation

--- ControllerException.cpp
+++ ControllerException.cpp
@@ -2,17 +2,24 @@
 
 #include "ControllerException.h"
 
-ControllerException::ControllerException(std::string customMessage)
+ControllerException::ControllerException(std::string customMessage) : customMessage(customMessage)
 {
-
+    hasBabushka = false;
 }
 
-ControllerException::ControllerException(BabushkaException& babushkaException, std::string customMessage)
+ControllerException::ControllerException(BabushkaException& babushkaException, std::string customMessage) : babushkaException(&babushkaException), customMessage(customMessage)
 {
-
+    hasBabushka = true;
 }
 
 void ControllerException::printMessage()
 {
-
+    if (hasBabushka)
+    {
+        babushkaException->printMessage();
+    }
+    else
+    {
+        std::cout<< customMessage <<std::endl;
+    }
 }

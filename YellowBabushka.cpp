89913336ca2ca05c95949f14bc4ea2712f9f4eb6
Ashley Kapaso <ashleykapaso@gmail.com>
2022-09-10 17:35:03 +0200

    YellowBabushka Impleamentation

--- YellowBabushka.cpp
+++ YellowBabushka.cpp
@@ -4,10 +4,32 @@ YellowBabushka::YellowBabushka(const unsigned char* id) : Babushka(id) {}
 
 void YellowBabushka::encrypt(unsigned char* data, int size)
 {
+    int start = 0;
+    int end = size-1;
 
+    while (start < end)
+    {
+        unsigned char temp = data[start];
+        data[start] = data[end];
+        data[end] = temp;
+        
+        start++;
+        end--;
+    }
 }
 
 void YellowBabushka::decrypt(unsigned char* data, int size)
 {
+    int start = 0;
+    int end = size-1;
 
+    while (start < end)
+    {
+        unsigned char temp = data[start];
+        data[start] = data[end];
+        data[end] = temp;
+
+        start++;
+        end--;
+    }
 }

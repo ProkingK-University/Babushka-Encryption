e4fabcba86801d4432f5c1db39bd13bae4848d0f
Ashley Kapaso <ashleykapaso@gmail.com>
2022-09-10 16:25:00 +0200

    GreenBabushka Impleamentation

--- GreenBabushka.cpp
+++ GreenBabushka.cpp
@@ -1,16 +1,51 @@
 #include "GreenBabushka.h"
 
-GreenBabushka::GreenBabushka(const unsigned char* id, unsigned int displacement) : Babushka(id)
-{
+#include "DisplacementException.h"
 
-}
+GreenBabushka::GreenBabushka(const unsigned char* id, unsigned int displacement) : Babushka(id), displacement(displacement) {}
 
 void GreenBabushka::encrypt(unsigned char* data, int size)
 {
+    if (displacement > size-2)
+    {
+        throw DisplacementException();
+    }
+    else
+    {
+        for (int i = 0; i < size; i++)
+        {
+            if (i+displacement < size)
+            {
+                unsigned char temp = data[i];
+                data[i] = data[i+displacement];
+                data[i+displacement] = temp;
 
+                i += displacement+1;
+            }
+        }
+    }
 }
 
 void GreenBabushka::decrypt(unsigned char* data, int size)
 {
-    
+    if (displacement > size-2)
+    {
+        throw DisplacementException();
+    }
+    else
+    {
+        for (int i = 0; i < size; i++)
+        {
+            if (i+displacement < size)
+            {
+                unsigned char temp = data[i];
+                data[i] = data[i+displacement];
+                data[i+displacement] = temp;
+
+                i += displacement+1;
+            }
+        }
+    }
 }
+
+// :)

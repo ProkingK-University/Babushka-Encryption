b811646ef27641fb960b49f344093d1c06d6c52c
Ashley Kapaso <ashleykapaso@gmail.com>
2022-09-10 15:48:32 +0200

    GoldBabushka Impleamentation

--- GoldBabushka.cpp
+++ GoldBabushka.cpp
@@ -1,16 +1,46 @@
 #include "GoldBabushka.h"
 
-GoldBabushka::GoldBabushka(const unsigned char* id, unsigned char xorValue) : Babushka(id)
-{
+#include "OverflowException.h"
+#include "UnderflowException.h"
 
-}
+GoldBabushka::GoldBabushka(const unsigned char* id, unsigned char xorValue) : Babushka(id), xorValue(xorValue) {}
 
 void GoldBabushka::encrypt(unsigned char* data, int size)
 {
-
+    for (int i = 0; i < size; i++)
+    {
+        if (data[i] ^ xorValue > MAX_VALUE_INCLUSIVE)
+        {
+            throw OverflowException();
+        }
+        else if (data[i] ^ xorValue < MIN_VALUE_INCLUSIVE)
+        {
+            throw UnderflowException();
+        }
+        else
+        {
+            data[i] ^= xorValue;
+        }
+    }
 }
 
 void GoldBabushka::decrypt(unsigned char* data, int size)
 {
-    
+    for (int i = 0; i < size; i++)
+    {
+        if (data[i] ^ xorValue > MAX_VALUE_INCLUSIVE)
+        {
+            throw OverflowException();
+        }
+        else if (data[i] ^ xorValue < MIN_VALUE_INCLUSIVE)
+        {
+            throw UnderflowException();
+        }
+        else
+        {
+            data[i] ^= xorValue;
+        }
+    }
 }
+
+// :)

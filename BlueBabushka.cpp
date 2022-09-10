bd4f1e8d3fcb7059f793f365b928d1633b3b7412
Ashley Kapaso <ashleykapaso@gmail.com>
2022-09-10 15:41:01 +0200

    BlueBabushka Impleamentation

--- BlueBabushka.cpp
+++ BlueBabushka.cpp
@@ -1,6 +1,3 @@
 #include "BlueBabushka.h"
 
-BlueBabushka::BlueBabushka(const unsigned char* id, unsigned int amount) : Babushka(id)
-{
-    
-}
+BlueBabushka::BlueBabushka(const unsigned char* id, unsigned int amount) : Babushka(id), amount(amount) {}
--- LightBlueBabushka.cpp
+++ LightBlueBabushka.cpp
@@ -1,13 +1,46 @@
 #include "LightBlueBabushka.h"
 
+#include "OverflowException.h"
+#include "UnderflowException.h"
+
 LightBlueBabushka::LightBlueBabushka(const unsigned char* id, unsigned int amount) : BlueBabushka(id, amount) {}
 
 void LightBlueBabushka::encrypt(unsigned char* data, int size)
 {
-
+    for (int i = 0; i < size; i++)
+    {
+        if (data[i] + amount > MAX_VALUE_INCLUSIVE)
+        {
+            throw OverflowException();
+        }
+        else if (data[i] + amount < MIN_VALUE_INCLUSIVE)
+        {
+            throw UnderflowException();
+        }
+        else
+        {
+            data[i] += amount;
+        }
+    }
 }
 
 void LightBlueBabushka::decrypt(unsigned char* data, int size)
 {
-    
+    for (int i = 0; i < size; i++)
+    {
+        if (data[i] - amount > MAX_VALUE_INCLUSIVE)
+        {
+            throw OverflowException();
+        }
+        else if (data[i] - amount < MIN_VALUE_INCLUSIVE)
+        {
+            throw UnderflowException();
+        }
+        else
+        {
+            data[i] -= amount;
+        }
+    }
 }
+
+// :)
